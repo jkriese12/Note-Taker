bbb028366f745e4376a4b38d739eea764aaf8931
Jacob Kriese <jkriese12@gmail.com>
2021-01-31 18:52:56 -0700

    Post request logic begun

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -1,25 +1,48 @@
-// Dependencies
+// Dependencies/ Directory
 // =============================================================
-var express = require("express");
-var path = require("path");
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const dbDir = path.resolve(__dirname, "db");
+const dbPath = path.join(dbDir, "db.json");
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = 3000;
+const app = express();
+const PORT = 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Read and write function
+// =============================================================
+const writeDb = (data) => {
+  fs.appendFile(dbPath, data, (err) =>
+    err ? console.error(err) : console.log("File written")
+  );
+};
+
 // Routes
 // =============================================================
-app.get("*", function (req, res) {
+app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.get("/notes", function (req, res) {
   res.sendFile(path.join(__dirname, "notes.html"));
+});
+
+app.get("/api/notes", function (req, res) {
+  fs.readFile(dbPath, (err) => (err ? console.error(err) : console.log("File written")));
+});
+
+// Posts
+// =============================================================
+app.post("api/notes", function (req, res) {
+  var newNote = req.body;
+  writeDb(newNote);
 });
 // Starts the server to begin listening
 // =============================================================
