0756eda1d514b88302fa1efb12d647de70f2bf7c
Jacob Kriese <jkriese12@gmail.com>
2021-01-31 18:12:41 -0700

    server.js file created. express boiler plate and routes added

---
+++ Develop/public/server.js
@@ -0,0 +1,28 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
+// =============================================================
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "notes.html"));
+});
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function () {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
